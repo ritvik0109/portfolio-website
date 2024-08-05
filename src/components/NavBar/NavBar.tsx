13d42e976e8b4b364d76a0fc252b23b138e052a0
Ritvik Malhotra <ritvikmalhotra0109@gmail.com>
2024-08-05 19:31:48 -0400

    add preliminary navbar

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import {
+  Box,
+  Flex,
+  Text,
+  Button,
+  Stack,
+  Popover,
+  PopoverTrigger,
+  useColorModeValue,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+export default function NavBar() {
+  return (
+    <Box>
+      <Flex
+        bg={useColorModeValue("white", "gray.800")}
+        color={useColorModeValue("gray.600", "white")}
+        minH={"60px"}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align={"center"}
+      >
+        <Text
+          textAlign={useBreakpointValue({ base: "center", md: "left" })}
+          fontFamily={"heading"}
+          color={useColorModeValue("gray.800", "white")}
+        >
+          Logo
+        </Text>
+
+        <Flex flex={{ base: 1 }} justify="center">
+          <DesktopNav />
+        </Flex>
+
+        {/* May want to change this button into a light/dark toggle */}
+        <Button
+          as={"a"}
+          display={{ base: "none", md: "inline-flex" }}
+          fontSize={"sm"}
+          fontWeight={600}
+          color={"white"}
+          bg={"pink.400"}
+          href={"#"}
+          _hover={{
+            bg: "pink.300",
+          }}
+        >
+          Sign Up
+        </Button>
+      </Flex>
+    </Box>
+  );
+}
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("gray.800", "white");
+
+  return (
+    <Stack direction={"row"} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box
+          key={navItem.label}
+          as="a"
+          p={2}
+          href={navItem.href ?? "#"}
+          fontSize={"sm"}
+          fontWeight={500}
+          color={linkColor}
+          _hover={{
+            textDecoration: "none",
+            color: linkHoverColor,
+          }}
+        >
+          {navItem.label}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+type NavItem = {
+  label: string;
+  href?: string;
+};
+
+const NAV_ITEMS: NavItem[] = [
+  {
+    label: "About Me",
+    href: "#",
+  },
+  {
+    label: "Portfolio",
+    href: "#",
+  },
+  {
+    label: "Contact",
+    href: "#",
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Landing from "@/components/Landing/Landing";
+import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
   return (
     <>
-      <Landing />
+      <NavBar />
+      {/* <Landing /> */}
     </>
   );
 }
