56e52bf8f318c06b675529c113b44219e00344ec
Ritvik Malhotra <ritvikmalhotra0109@gmail.com>
2024-08-06 20:32:53 -0400

    refactor: moved navbar and footer to _app.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import customTheme from "../theme";
+import Footer, { SmallFooter } from "@/components/Footer/Footer";
+import NavBar from "@/components/NavBar/NavBar";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
+      <NavBar />
       <Component {...pageProps} />
+      <Footer />
+      {/* <SmallFooter /> */}
     </ChakraProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import Footer, { SmallFooter } from "@/components/Footer/Footer";
 import Landing from "@/components/Landing/Landing";
-import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
-  return (
-    <>
-      <NavBar />
-      <Landing />
-      <Footer />
-      {/* <SmallFooter /> */}
-    </>
-  );
+  return <Landing />;
 }
