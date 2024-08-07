36dacf76bd73c425038dfaa725bffccb073c004f
Ritvik Malhotra <ritvikmalhotra0109@gmail.com>
2024-08-06 20:31:12 -0400

    add contact me section

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  IconButton,
+  Button,
+  VStack,
+  HStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+} from "@chakra-ui/react";
+import { MdPhone, MdEmail, MdLocationOn, MdOutlineEmail } from "react-icons/md";
+import { BsGithub, BsPerson } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+
+export default function Contact() {
+  return (
+    <Flex>
+      <Box
+        bg="#02054B"
+        color="white"
+        borderRadius="lg"
+        m={{ sm: 4, md: 16, lg: 10 }}
+        p={{ sm: 5, md: 5, lg: 16 }}
+      >
+        <Box p={4}>
+          <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+            <WrapItem>
+              <Box>
+                <Heading pt={{ base: 4, md: 12 }}>Let&#39;s Talk</Heading>
+                <Text mt={{ base: 3, md: 3, lg: 2 }} color="gray.500">
+                  New projects, freelance inqury or even a coffee.
+                </Text>
+                <Box
+                  pt={{ base: 8, sm: 8, md: 10, lg: 12 }}
+                  pb={{ base: 7, sm: 7, md: 8 }}
+                >
+                  <VStack pl={0} spacing={3} alignItems="center">
+                    <Button
+                      size="md"
+                      height="48px"
+                      width="300px"
+                      variant="ghost"
+                      color="#DCE2FF"
+                      _hover={{ border: "2px solid #1C6FEB" }}
+                      leftIcon={<MdPhone color="#1970F1" size="20px" />}
+                    >
+                      +1 (647) 877-0965
+                    </Button>
+                    <Button
+                      size="md"
+                      height="48px"
+                      width="300px"
+                      variant="ghost"
+                      color="#DCE2FF"
+                      _hover={{ border: "2px solid #1C6FEB" }}
+                      leftIcon={<MdEmail color="#1970F1" size="20px" />}
+                    >
+                      ritvikmalhotra0109@gmail.com
+                    </Button>
+                    <Button
+                      size="md"
+                      height="48px"
+                      width="300px"
+                      variant="ghost"
+                      color="#DCE2FF"
+                      _hover={{ border: "2px solid #1C6FEB" }}
+                      leftIcon={<MdLocationOn color="#1970F1" size="20px" />}
+                    >
+                      Toronto, Canada
+                    </Button>
+                  </VStack>
+                </Box>
+                <HStack spacing={5} px={5} justifyContent="center">
+                  <IconButton
+                    aria-label="github"
+                    variant="ghost"
+                    size="lg"
+                    isRound={true}
+                    _hover={{ bg: "#0D74FF" }}
+                    icon={<BsGithub size="28px" />}
+                  />
+                  <IconButton
+                    aria-label="facebook"
+                    variant="ghost"
+                    size="lg"
+                    isRound={true}
+                    _hover={{ bg: "#0D74FF" }}
+                    icon={<FaLinkedin size="28px" />}
+                  />
+                </HStack>
+              </Box>
+            </WrapItem>
+            <WrapItem>
+              <Box bg="white" borderRadius="lg">
+                <Box m={8} color="#0B0E3F">
+                  <VStack spacing={5}>
+                    <FormControl id="name">
+                      <FormLabel>Your Name</FormLabel>
+                      <InputGroup borderColor="#E0E1E7">
+                        <InputLeftElement pointerEvents="none">
+                          <BsPerson color="gray.800" />
+                        </InputLeftElement>
+                        <Input type="text" size="md" />
+                      </InputGroup>
+                    </FormControl>
+                    <FormControl id="name">
+                      <FormLabel>Mail</FormLabel>
+                      <InputGroup borderColor="#E0E1E7">
+                        <InputLeftElement pointerEvents="none">
+                          <MdOutlineEmail color="gray.800" />
+                        </InputLeftElement>
+                        <Input type="text" size="md" />
+                      </InputGroup>
+                    </FormControl>
+                    <FormControl id="name">
+                      <FormLabel>Message</FormLabel>
+                      <Textarea
+                        borderColor="gray.300"
+                        _hover={{
+                          borderRadius: "gray.300",
+                        }}
+                        placeholder="message"
+                      />
+                    </FormControl>
+                    <FormControl id="name" float="right">
+                      <Button
+                        variant="solid"
+                        bg="#0D74FF"
+                        color="white"
+                        _hover={{}}
+                      >
+                        Send Message
+                      </Button>
+                    </FormControl>
+                  </VStack>
+                </Box>
+              </Box>
+            </WrapItem>
+          </Wrap>
+        </Box>
+      </Box>
+    </Flex>
+  );
+}
