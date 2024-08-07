6b696ad10b1098d8b8e544d5a1cb4616d7d1db7f
Ritvik Malhotra <ritvikmalhotra0109@gmail.com>
2024-08-06 20:32:12 -0400

    feat: add basic structure to landing page

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,5 +1,31 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import Contact from "../Contact/Contact";
 import Hero from "../Hero/Hero";
 
 export default function Landing() {
-  return <Hero />;
+  return (
+    <Flex direction="column" align="center" justify="center" minH="100vh" p={4}>
+      <Box textAlign="center" mb={10}>
+        <Hero />
+      </Box>
+
+      <Box textAlign="center" mb={10}>
+        <Heading as="h2" size="xl" mb={4}>
+          About Me
+        </Heading>
+        <Text fontSize="lg">Brief introduction about yourself.</Text>
+      </Box>
+
+      <Box textAlign="center" mb={10}>
+        <Heading as="h2" size="xl" mb={4}>
+          Projects
+        </Heading>
+        <Text fontSize="lg">Some of your key projects.</Text>
+      </Box>
+
+      <Box textAlign="center" mb={10}>
+        <Contact />
+      </Box>
+    </Flex>
+  );
 }
