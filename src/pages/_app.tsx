b9ec8edc38c9684d44a0fc34ed4048bea35c8601
Ritvik Malhotra <ritvikmalhotra0109@gmail.com>
2024-08-10 22:37:32 -0400

    fix: makes footer sticky to bottom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import customTheme from "../theme";
 import Footer, { SmallFooter } from "@/components/Footer/Footer";
@@ -7,10 +7,14 @@ import NavBar from "@/components/NavBar/NavBar";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={customTheme}>
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
-      {/* <SmallFooter /> */}
+      <Flex direction="column" minH="100vh">
+        <NavBar />
+        <Flex flex="1">
+          <Component {...pageProps} />
+        </Flex>
+        <Footer />
+        {/* <SmallFooter /> */}
+      </Flex>
     </ChakraProvider>
   );
 }
