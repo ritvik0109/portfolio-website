3ebbfef3374104c997b2dbba30635e688c07f859
Ritvik Malhotra <ritvikmalhotra0109@gmail.com>
2024-08-20 15:14:21 -0400

    style: updates social icon sizes, and transitions on navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,7 +7,6 @@ import {
   Stack,
   useColorModeValue,
   useColorMode,
-  useBreakpointValue,
   Link,
   Divider,
 } from "@chakra-ui/react";
@@ -39,80 +38,94 @@ const NAV_ITEMS: NavItem[] = [
 
 export default function NavBar() {
   const { colorMode, toggleColorMode } = useColorMode();
-  const linkColor = useColorModeValue("gray.600", "gray.200");
   const linkHoverColor = useColorModeValue("gray.800", "#099cff");
-  const iconSize = useBreakpointValue({
-    base: "6vw",
-    sm: "4vw",
-    md: "3vw",
-    lg: "2vw",
-  });
 
   return (
-    <Box width="100%">
+    <Box width="100%" px={{ base: 4, md: 8, lg: 12 }}>
       <Flex
         bg={useColorModeValue("white", "background")}
-        color={useColorModeValue("gray.600", "white")}
+        color={useColorModeValue("gray.600", "#DCE2FF")}
         minH={"60px"}
         py={{ base: 2 }}
-        px={{ base: 4 }}
         borderBottom={1}
         align={"center"}
       >
         <Logo />
-        <Flex flex={{ base: 1 }} justify="center">
-          <Stack direction={"row"} spacing={4}>
-            {NAV_ITEMS.map((navItem) => (
-              <Box
-                key={navItem.label}
-                as="a"
-                p={2}
-                href={navItem.href ?? "#"}
-                fontSize={"sm"}
-                fontWeight={500}
-                color={linkColor}
-                _hover={{
-                  color: linkHoverColor,
-                  transform: "scale(1.1)",
-                  transition: "all 0.4s ease-in-out",
-                }}
-              >
-                {navItem.label}
-              </Box>
-            ))}
-          </Stack>
-        </Flex>
 
-        <Box display="flex" justifyContent="center" alignItems="center" gap="6">
-          <Link href={content.contact.github} isExternal aria-label="GitHub">
-            <Box
-              as={FaGithub}
-              boxSize={iconSize}
-              transition="transform 0.2s"
-              _hover={{ transform: "scale(1.2)" }}
-            />
-          </Link>
-          <Link
-            href={content.contact.linkedin}
-            isExternal
-            aria-label="LinkedIn"
+        <Flex
+          display={{ base: "none", md: "flex" }}
+          alignItems="center"
+          justifyContent="space-between"
+          flexGrow={1}
+        >
+          <Flex flex={{ base: 1 }} justify="center">
+            <Stack direction={"row"} spacing={4}>
+              {NAV_ITEMS.map((navItem) => (
+                <Box
+                  key={navItem.label}
+                  as="a"
+                  p={3}
+                  href={navItem.href ?? "#"}
+                  fontWeight={500}
+                  _hover={{
+                    color: linkHoverColor,
+                    transform: "scale(1.1)",
+                    transition: "all 0.4s ease-in-out",
+                  }}
+                >
+                  {navItem.label}
+                </Box>
+              ))}
+            </Stack>
+          </Flex>
+
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            gap="6"
           >
-            <Box
-              as={FaLinkedin}
-              boxSize={iconSize}
-              transition="transform 0.2s"
-              _hover={{ transform: "scale(1.2)" }}
-            />
-          </Link>
-          <Link href={"mailto:" + content.contact.email} aria-label="Email">
-            <Box
-              as={MdEmail}
-              boxSize={iconSize}
-              transition="transform 0.2s"
-              _hover={{ transform: "scale(1.2)" }}
-            />
-          </Link>
-          {/* TODO: Uncomment toggle for Light mode once colour scheme ready  */}
+            <Link href={content.contact.github} isExternal aria-label="GitHub">
+              <Box
+                as={FaGithub}
+                boxSize={{ base: "32px", lg: "36px" }}
+                transition="transform 0.2s"
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "all 0.4s ease-in-out",
+                  color: linkHoverColor,
+                }}
+              />
+            </Link>
+            <Link
+              href={content.contact.linkedin}
+              isExternal
+              aria-label="LinkedIn"
+            >
+              <Box
+                as={FaLinkedin}
+                boxSize={{ base: "32px", lg: "36px" }}
+                transition="transform 0.2s"
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "all 0.4s ease-in-out",
+                  color: linkHoverColor,
+                }}
+              />
+            </Link>
+            <Link href={"mailto:" + content.contact.email} aria-label="Email">
+              <Box
+                as={MdEmail}
+                boxSize={{ base: "32px", lg: "36px" }}
+                transition="transform 0.2s"
+                _hover={{
+                  transform: "scale(1.2)",
+                  transition: "all 0.4s ease-in-out",
+                  color: linkHoverColor,
+                }}
+              />
+            </Link>
+            {/* TODO: Uncomment toggle for Light mode once colour scheme ready  */}
             {/* <Divider
               orientation="vertical"
               h={"28px"}
@@ -121,14 +134,15 @@ export default function NavBar() {
               colorScheme={useColorModeValue("black", "white")}
             />
             <Box
-            as={colorMode === "light" ? MoonIcon : SunIcon}
-            boxSize={iconSize}
-            transition="transform 0.2s"
-            _hover={{ transform: "scale(1.2)", cursor: "pointer" }}
-            onClick={toggleColorMode}
-            aria-label="Toggle color mode"
-          /> */}
-        </Box>
+              as={colorMode === "light" ? MoonIcon : SunIcon}
+              boxSize={{ base: "32px", lg: "36px" }}
+              transition="transform 0.2s"
+              _hover={{ transform: "scale(1.2)", cursor: "pointer" }}
+              onClick={toggleColorMode}
+              aria-label="Toggle color mode"
+            /> */}
+          </Box>
+        </Flex>
       </Flex>
     </Box>
   );
