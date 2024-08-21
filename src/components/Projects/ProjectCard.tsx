88c7dad396c649c8560c4663189b49e43e2d4b9a
Ritvik Malhotra <ritvikmalhotra0109@gmail.com>
2024-08-20 20:29:06 -0400

    fix: make project cards clickable with github links

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -7,10 +7,9 @@ import {
   Heading,
   Text,
   Stack,
-  Avatar,
   useColorModeValue,
-  HStack,
   Wrap,
+  Link,
 } from "@chakra-ui/react";
 
 type ProjectCardProps = {
@@ -18,7 +17,6 @@ type ProjectCardProps = {
   description: string;
   githubLink: string;
   projectUrl?: string;
-  //   FIXME
   image?: string;
   techStack: string[];
 };
@@ -33,48 +31,50 @@ export default function ProjectCard({
 }: ProjectCardProps) {
   return (
     <Center py={6}>
-      <Box
-        maxW={"445px"}
-        w={"full"}
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        bg={useColorModeValue("white", "#353754")}
-        boxShadow={"2xl"}
-        rounded={"md"}
-        p={6}
-        overflow={"hidden"}
-      >
+      <Link href={githubLink} isExternal _hover={{ textDecoration: "none" }}>
         <Box
-          h={"210px"}
-          bg={"gray.100"}
-          mt={-6}
-          mx={-6}
-          mb={6}
-          pos={"relative"}
+          maxW={"445px"}
+          w={"full"}
+          // eslint-disable-next-line react-hooks/rules-of-hooks
+          bg={useColorModeValue("white", "#353754")}
+          boxShadow={"2xl"}
+          rounded={"md"}
+          p={6}
+          overflow={"hidden"}
         >
-          <Image src={"/images/developer.png"} fill alt="Example" />
+          <Box
+            h={"210px"}
+            bg={"gray.100"}
+            mt={-6}
+            mx={-6}
+            mb={6}
+            pos={"relative"}
+          >
+            <Image src={image || "/images/developer.png"} fill alt={title} />
+          </Box>
+          <Stack flexGrow={1} justify="space-between">
+            <Wrap justify="center" spacing="3">
+              {techStack.map((item, index) => (
+                <Text
+                  key={index}
+                  color={"#099cff"}
+                  textTransform={"uppercase"}
+                  fontWeight={800}
+                  fontSize={"sm"}
+                  letterSpacing={1.1}
+                >
+                  {item}
+                </Text>
+              ))}
+            </Wrap>
+            <Heading fontSize={"2xl"} fontFamily={"body"} mt={1} mb={2}>
+              {title}
+            </Heading>
+            <Text>{description}</Text>
+            {/* Github and live links? Be lazy and just make the button go to the github? and the readme on github should contain the live links lolololololol*/}
+          </Stack>
         </Box>
-        <Stack flexGrow={1} justify="space-between">
-          <Wrap justify="center" spacing="3">
-            {techStack.map((item, index) => (
-              <Text
-                key={index}
-                color={"#099cff"}
-                textTransform={"uppercase"}
-                fontWeight={800}
-                fontSize={"sm"}
-                letterSpacing={1.1}
-              >
-                {item}
-              </Text>
-            ))}
-          </Wrap>
-          <Heading fontSize={"2xl"} fontFamily={"body"} mt={1} mb={2}>
-            {title}
-          </Heading>
-          <Text>{description}</Text>
-          {/* Github and live links? Be lazy and just make the button go to the github? and the readme on github should contain the live links lolololololol*/}
-        </Stack>
-      </Box>
+      </Link>
     </Center>
   );
 }
